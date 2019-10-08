d08f229556f74d69e2c449f275932bee5df2719a
ayan4m1 <andrew@bulletlogic.com>
2019-10-08 16:06:20 -0400

    add initial implementation for ELR->ATF importer

--- src/atf.js
+++ src/atf.js
@@ -6,6 +6,8 @@ import { createReadStream } from 'fs';
 import loggers from './logging';
 
 const log = loggers('app');
+const flavorNamePattern = /(.*)\(([A-Z0-9]+)\)/i;
+const stashUrlPattern = /https:\/\/alltheflavors.com\/my\/flavors\/([A-Z0-9-]+)\/stash/i;
 
 export default async csv => {
   const browser = await puppeteer.launch({
@@ -16,14 +18,48 @@ export default async csv => {
 
   try {
     const stream = createReadStream(csv);
-    const data = getStream(stream);
-    const flavors = await neatCsv(data);
-
-    // eslint-disable-next-line
-    console.dir(flavors);
+    const data = await getStream(stream);
+    const flavors = await neatCsv(data, {
+      separator: ';'
+    });
 
     await page.goto('https://alltheflavors.com');
     log.info('Waiting for login...');
+    await page.waitForSelector('a[href="/recipes?owner=both"]', {
+      timeout: 0
+    });
+
+    log.info('Logged in successfully!');
+    await page.goto('https://alltheflavors.com/flavors', {
+      waitUntil: ['load', 'networkidle0']
+    });
+    for (const flavor of flavors) {
+      const $input = await page.$('#name_like');
+      const { Flavor: flavorSlug } = flavor;
+      const [, rawName, rawVendor] = flavorSlug.match(flavorNamePattern);
+      const searchTerm = `${rawVendor} ${rawName}`;
+
+      page.evaluate(input => {
+        input.value = '';
+      }, $input);
+      $input.type(searchTerm);
+      log.info('Waiting for results...');
+      await page.waitForResponse(
+        response =>
+          response
+            .url()
+            .startsWith('https://alltheflavors.com/flavors/live_search'),
+        { timeout: 0 }
+      );
+      log.info('Check a box or hit reload to skip...');
+
+      await Promise.race([
+        page.waitForResponse(response => stashUrlPattern.test(response.url()), {
+          timeout: 0
+        }),
+        page.waitForNavigation({ timeout: 0, waitUntil: ['load'] })
+      ]);
+    }
   } catch (error) {
     log.error(error.message, error);
 
